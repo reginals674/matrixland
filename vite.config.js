4605d0302a7920b283f59c1a372845b396bb519a
reginals674 <132300862+reginals674@users.noreply.github.com>
2025-11-14 14:39:57 -0600

    .

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/matrixland/',
+  build: {
+    outDir: 'dist'
+  }
+})
